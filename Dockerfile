8e065307433e80ca54cfa4abfa185e4f53ff2cd3
Talha Mujahid <talha.mujahid@wearenova.co.uk>
2024-03-24 23:54:10 +0500

    replace with code

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
-FROM node:lts
-# Or whatever Node version/image you want
-WORKDIR '/var/www/app'
+
+FROM node:lts-alpine3.14 AS base
+WORKDIR /app
+COPY package*.json  ./
+RUN npm install
+COPY . ./
+RUN npm prune --production # Remove dev dependencies
+EXPOSE 80
+CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -1,27 +1,20 @@
+// server.js
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 80;
 
-
-const redisClient = require('./redis-client');
-
-app.get('/store/:key', async (req, res) => {
-  const { key } = req.params;
-  const value = req.query;
-  await redisClient.setAsync(key, JSON.stringify(value));
-  return res.send('Success');
+// Define the health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).send('api is running ... healthy');
 });
 
-app.get('/:key', async (req, res) => {
-  const { key } = req.params;
-  const rawData = await redisClient.getAsync(key);
-  return res.json(JSON.parse(rawData));
-});
-
+// Define the root endpoint
 app.get('/', (req, res) => {
-  return res.send('Hello world');
+  res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
